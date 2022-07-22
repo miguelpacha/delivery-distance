2bc50b3c47809a080384a07c33ac01d67ca27b35
Miguel Pachá <>
2022-07-22 15:49:35 -0300

    feat: distance function using haversine formula

---
+++ app/geography.py
@@ -0,0 +1,15 @@
+from math import radians, sin, asin, cos, sqrt
+
+EARTH_RADIUS = 6371 # https://en.wikipedia.org/wiki/Earth_radius
+
+def distance(_lat1, _lon1, _lat2, _lon2):
+    # https://en.wikipedia.org/wiki/Haversine_formula#Formulation
+    
+    lat1, lon1, lat2, lon2 = (radians(angle) for angle in (_lat1, _lon1, _lat2, _lon2))
+    
+    delta_lat = lat2 - lat1
+    delta_lon = lon2 - lon1
+    
+    theta = asin(sqrt(sin(delta_lat/2)**2 + cos(lat1)*cos(lat2) * sin(delta_lon/2)**2))
+
+    return 2 * EARTH_RADIUS * theta
